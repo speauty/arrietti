911a0ed545cf98ade53db0976aabae954730eeb8
speauty <speauty@163.com>
2024-08-08 09:06:24 +0800

    更新: 增加关于弹窗, 显示数据模拟了部分;

--- electron/config.ts
+++ electron/config.ts
@@ -1,24 +1,49 @@
-import { BrowserWindow } from "electron/main"
+import { BrowserWindow, dialog } from "electron"
 
 const isMac = process.platform === 'darwin'
 
 export const menus = (win: BrowserWindow): Array<(Electron.MenuItemConstructorOptions) | (Electron.MenuItem)> => {
     return [
-        { label: "文件(F)(&F)", submenu: [
-            {label: "退出(&Q)", role: isMac?'close':'quit'}
-        ]},
-        {label: "首选项(R)(&R)", submenu: [
-            {label: "首页", click: () => sendRouteToRenderer(win, "home")},
-            {label: "其他", click: () => sendRouteToRenderer(win, "other")},
-        ]},
-        {label: "帮助(H)(&H)", submenu: [
-            {label: "刷新", role: "reload"},
-            {label: "强制刷新", role: "forceReload"},
-            {label: "调试面板", role: "toggleDevTools"}
-        ]},
+        {
+            label: "文件(F)(&F)", submenu: [
+                { label: "退出(&Q)", role: isMac ? 'close' : 'quit' }
+            ]
+        },
+        {
+            label: "首选项(R)(&R)", submenu: [
+                { label: "首页", click: () => sendRouteToRenderer(win, "home") },
+                { label: "其他", click: () => sendRouteToRenderer(win, "other") },
+            ]
+        },
+        {
+            label: "帮助(H)(&H)", submenu: [
+                { label: "刷新", role: "reload" },
+                { label: "强制刷新", role: "forceReload" },
+                { label: "调试面板", role: "toggleDevTools" },
+                { label: "关于", click: () => dialogForAbout(win) },
+            ]
+        },
     ]
 }
 
 function sendRouteToRenderer(win: BrowserWindow, routeName: string) {
     win?.webContents.send('route-change', routeName)
-  }
+}
+
+const dialogForAbout = (win: BrowserWindow) => {
+    dialog.showMessageBox(win, {
+        type: 'info',
+        title: process.env.VITE_APP_NAME?process.env.VITE_APP_NAME:"阿莉埃蒂",
+        message: process.env.VITE_APP_NAME?process.env.VITE_APP_NAME:"阿莉埃蒂",
+// 提交: e170252f762678dec6ca2cc69aba1570769a5d39
+// 日期: 2024-04-10T17:41:02.734Z
+        detail: `版本: ${process.env.npm_package_version}
+Electron: ${process.versions.electron}
+ElectronBuildId: ${process.env.ELECTRON_DIST_URL}
+Chromium: ${process.versions.chrome}
+Node.js: ${process.versions.node}
+V8: ${process.versions.v8}
+OS: ${process.platform} ${process.arch} ${process.getSystemVersion() }
+`
+    })
+}
